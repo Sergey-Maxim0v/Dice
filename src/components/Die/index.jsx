dda461b5658222f15af56a69035ed80c14e200aa
Seregey_Maximov <dev9092336062@gmail.com>
2022-04-05 11:22:45 +0300

    handleInput

--- src/components/Die/index.jsx
+++ src/components/Die/index.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, {useCallback} from "react";
 import styles from "./styles.module.scss"
 
 const Die = ({die, setDiceArray, index, diceArray}) => {
+
+  const handleInput = useCallback(
+      (event, input) => {
+        const userDie = {name: die.name.slice(), value: die.value.slice()}
+        const userDiceArray = diceArray.slice();
+
+        if (input === 'name') {
+          userDiceArray[index] = {...userDie, name: event.target.value}
+        }
+
+        if (input === 'value') {
+          userDiceArray[index] = {...userDie, value: event.target.value.split(',')}
+        }
+        console.log(userDiceArray)
+
+        return setDiceArray(userDiceArray);
+      },
+      [die, diceArray, setDiceArray, index]
+  );
+
   return (
       <div className={styles.dieRow}>
         <h5 className={styles.dieTitle}>
           {`Die # ${index + 1}`}
         </h5>
-        <label>
-          <input type={'text'}
-                 placeholder={`Die name`}
-                 value={die.name}
-                 onChange={event => {
-                   return setDiceArray([...diceArray])
-                 }}
-          />
-        </label>
+        <input type={'text'}
+               placeholder={`Die name`}
+               value={die.name}
+               onChange={event => handleInput(event, 'name')}
+        />
         <label>
           <input type={'text'}
                  placeholder={`Fill in dice values separated by commas`}
                  value={die.value}
-                 onChange={(event) => {
-                   return setDiceArray([...diceArray]);
-                 }}
+                 onChange={event => handleInput(event, 'value')}
           />
         </label>
       </div>
